4b5ec743fefc20e96ca323eb092e1b1148403788
Kline <klinedylan46@gmail.com>
2025-12-07 23:03:21 -0600

    day 1 part 2 solution

--- Day_1/day1_solution.js
+++ Day_1/day1_solution.js
@@ -2,25 +2,28 @@ const fs = require('fs');
 
 const rotations = fs.readFileSync('input.txt', 'utf8').trim().split('\n');
 
-// Password = num of times the dial is left pointing at 0 after any rotation
+// Password = num of times the dial passes 0 during and after rotations
 let position = 50;
-let number_of_starts = 0;
+let number_of_zero = 0;
 for (const rotation of rotations) {
     const direction = rotation[0];
     const rotation_amount = parseInt(rotation.slice(1));
 
     if (direction === 'L') {
+        if (position === 0) {
+            number_of_zero += Math.floor(rotation_amount / 100);
+        } else if (rotation_amount >= position) {
+            number_of_zero += Math.floor((rotation_amount - position) / 100) + 1;
+        }
         position -= rotation_amount;
     }
     else {
+        number_of_zero += Math.floor((position + rotation_amount) / 100);
         position += rotation_amount;
     }
 
     position = ((position % 100) + 100) % 100;
 
-    if (position === 0) {
-        number_of_starts++;
-    }
 }
 
-console.log("Number of times 0 was seen: ", number_of_starts);
+console.log("Number of times 0 was seen: ", number_of_zero);
