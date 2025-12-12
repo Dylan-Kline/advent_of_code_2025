0a06a9fad002e92cce444fc8ab14884c9d9b4b0a
Kline <klinedylan46@gmail.com>
2025-12-11 20:13:11 -0600

    day 2 part 2 completed

--- day_2/day_2_solution_part_2.py
+++ day_2/day_2_solution_part_2.py
@@ -6,18 +6,14 @@ with open('input.txt', 'r') as f:
 raw_ranges = text.split(',')
 parsed_ranges = [r.split('-') for r in raw_ranges]
 
+def isRepeatingSubstring(s):
+    return s in (s+s)[1:-1]
+
 invalid_ids = []
 for start, end in parsed_ranges:
     for num in range(int(start), int(end)+1):
-        num = str(num)
-        n = len(num)
-        left = 0
-        for right in range(0, (n//2)):
-            size = right - left
-            first = num[left:right]
-            second = num[right:right+size]
-            
-            while right < (n//2) and first == second:
+        str_num = str(num)
+        if isRepeatingSubstring(str_num):
+            invalid_ids.append(num)
                 
-            
 print(sum(invalid_ids))
